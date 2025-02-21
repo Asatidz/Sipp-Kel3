7c029bf00896e0a087a9b4fb47d6ca7eb3db4d94
Asatidz <asatidzal.hakiim3@smk.belajar.id>
2025-02-21 11:22:13 +0700

    okeeeeeeeee

--- dbfix.sql
+++ dbfix.sql
@@ -16,20 +16,18 @@
 
 
 -- Dumping database structure for sipp
-DROP DATABASE IF EXISTS `sipp`;
-CREATE DATABASE IF NOT EXISTS `sipp` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `sipp`;
+
 
 -- Dumping structure for table sipp.pelanggaran
 DROP TABLE IF EXISTS `pelanggaran`;
 CREATE TABLE IF NOT EXISTS `pelanggaran` (
   `id_pelanggaran` int NOT NULL AUTO_INCREMENT,
-  `nama_pelanggaran` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `jenis_pelanggaran` enum('ringan','sedang','berat') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `nama_pelanggaran` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `jenis_pelanggaran` enum('ringan','sedang','berat') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `poin` int DEFAULT NULL,
   `kode` int DEFAULT NULL,
   PRIMARY KEY (`id_pelanggaran`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Data exporting was unselected.
 
@@ -39,7 +37,7 @@ CREATE TABLE IF NOT EXISTS `riwayat_pelanggaran` (
   `id_riwayat` int NOT NULL AUTO_INCREMENT,
   `id_pelanggaran` int DEFAULT NULL,
   `id_siswa` int DEFAULT NULL,
-  `nama` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `nama` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `tanggal_riw` date DEFAULT NULL,
   `nis` int DEFAULT NULL,
   `kode_pl` int DEFAULT NULL,
@@ -51,7 +49,7 @@ CREATE TABLE IF NOT EXISTS `riwayat_pelanggaran` (
   KEY `fk_riwayat_siswa` (`id_siswa`,`nis`) USING BTREE,
   CONSTRAINT `fk_riwayat_pelanggaran` FOREIGN KEY (`id_pelanggaran`) REFERENCES `pelanggaran` (`id_pelanggaran`) ON DELETE CASCADE,
   CONSTRAINT `fk_riwayat_siswa` FOREIGN KEY (`id_siswa`) REFERENCES `siswa` (`id_siswa`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=70 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=70 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Data exporting was unselected.
 
@@ -65,7 +63,7 @@ CREATE TABLE IF NOT EXISTS `rw_tebus` (
   `kode` int DEFAULT NULL,
   `poin` int DEFAULT NULL,
   PRIMARY KEY (`id_rwt`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Data exporting was unselected.
 
@@ -74,15 +72,15 @@ DROP TABLE IF EXISTS `siswa`;
 CREATE TABLE IF NOT EXISTS `siswa` (
   `id_siswa` int NOT NULL AUTO_INCREMENT,
   `foto` varchar(1000) NOT NULL,
-  `nama_siswa` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `nama_siswa` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `nis` int NOT NULL,
   `tanggal_lahir` date NOT NULL,
   `kelas` enum('10','11','12') NOT NULL,
   `jurusan` enum('AKL','PPLG') NOT NULL,
-  `gender` enum('L','P') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `gender` enum('L','P') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `poin_sis` int(10) unsigned zerofill DEFAULT '0000000000',
   PRIMARY KEY (`id_siswa`)
-) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Data exporting was unselected.
 
@@ -90,11 +88,11 @@ CREATE TABLE IF NOT EXISTS `siswa` (
 DROP TABLE IF EXISTS `tebus`;
 CREATE TABLE IF NOT EXISTS `tebus` (
   `id_tebus` int NOT NULL AUTO_INCREMENT,
-  `nama_tebus` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `nama_tebus` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `poin_tebus` int DEFAULT NULL,
   `kode_tebus` int DEFAULT NULL,
   PRIMARY KEY (`id_tebus`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Data exporting was unselected.
 
@@ -107,7 +105,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `password` varchar(100) DEFAULT NULL,
   `role` enum('55','66','77','88') DEFAULT NULL,
   PRIMARY KEY (`id_user`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Data exporting was unselected.
 
