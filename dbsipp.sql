2b2e29362d46abcabd904730a38909d5316d1d82
Asatidz <asatidzal.hakiim3@smk.belajar.id>
2025-02-21 10:45:35 +0700

    tmbah

---
+++ dbsipp.sql
@@ -0,0 +1,112 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               8.0.30 - MySQL Community Server - GPL
+-- Server OS:                    Win64
+-- HeidiSQL Version:             12.1.0.6537
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Dumping structure for table sipp.pelanggaran
+DROP TABLE IF EXISTS `pelanggaran`;
+CREATE TABLE IF NOT EXISTS `pelanggaran` (
+  `id_pelanggaran` int NOT NULL AUTO_INCREMENT,
+  `nama_pelanggaran` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `jenis_pelanggaran` enum('ringan','sedang','berat') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `poin` int DEFAULT NULL,
+  `kode` int DEFAULT NULL,
+  PRIMARY KEY (`id_pelanggaran`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table sipp.riwayat_pelanggaran
+DROP TABLE IF EXISTS `riwayat_pelanggaran`;
+CREATE TABLE IF NOT EXISTS `riwayat_pelanggaran` (
+  `id_riwayat` int NOT NULL AUTO_INCREMENT,
+  `id_pelanggaran` int DEFAULT NULL,
+  `id_siswa` int DEFAULT NULL,
+  `nama` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `tanggal_riw` date DEFAULT NULL,
+  `nis` int DEFAULT NULL,
+  `kode_pl` int DEFAULT NULL,
+  `poin_plg` int DEFAULT NULL,
+  PRIMARY KEY (`id_riwayat`),
+  KEY `fk_riwayat_pelanggaran` (`id_pelanggaran`),
+  KEY `kode` (`kode_pl`) USING BTREE,
+  KEY `nis` (`nis`) USING BTREE,
+  KEY `fk_riwayat_siswa` (`id_siswa`,`nis`) USING BTREE,
+  CONSTRAINT `fk_riwayat_pelanggaran` FOREIGN KEY (`id_pelanggaran`) REFERENCES `pelanggaran` (`id_pelanggaran`) ON DELETE CASCADE,
+  CONSTRAINT `fk_riwayat_siswa` FOREIGN KEY (`id_siswa`) REFERENCES `siswa` (`id_siswa`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=70 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table sipp.rw_tebus
+DROP TABLE IF EXISTS `rw_tebus`;
+CREATE TABLE IF NOT EXISTS `rw_tebus` (
+  `id_rwt` int NOT NULL AUTO_INCREMENT,
+  `nis` int DEFAULT NULL,
+  `nama` varchar(50) DEFAULT NULL,
+  `tanggal` date DEFAULT NULL,
+  `kode` int DEFAULT NULL,
+  `poin` int DEFAULT NULL,
+  PRIMARY KEY (`id_rwt`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table sipp.siswa
+DROP TABLE IF EXISTS `siswa`;
+CREATE TABLE IF NOT EXISTS `siswa` (
+  `id_siswa` int NOT NULL AUTO_INCREMENT,
+  `foto` varchar(1000) NOT NULL,
+  `nama_siswa` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `nis` int NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `kelas` enum('10','11','12') NOT NULL,
+  `jurusan` enum('AKL','PPLG') NOT NULL,
+  `gender` enum('L','P') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `poin_sis` int(10) unsigned zerofill DEFAULT '0000000000',
+  PRIMARY KEY (`id_siswa`)
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table sipp.tebus
+DROP TABLE IF EXISTS `tebus`;
+CREATE TABLE IF NOT EXISTS `tebus` (
+  `id_tebus` int NOT NULL AUTO_INCREMENT,
+  `nama_tebus` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `poin_tebus` int DEFAULT NULL,
+  `kode_tebus` int DEFAULT NULL,
+  PRIMARY KEY (`id_tebus`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table sipp.user
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id_user` int NOT NULL AUTO_INCREMENT,
+  `nama_lengkap` varchar(50) DEFAULT NULL,
+  `username` varchar(30) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `role` enum('55','66','77','88') DEFAULT NULL,
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Data exporting was unselected.
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
